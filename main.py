f2977922440be33fe414b800e813e2b4fe7f1936
r4bb1t <r4bb1t.github@gmail.com>
2023-11-09 16:07:23 +0900

    feat: add cv2 and qt

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
+
+import sys
 import cv2
 from print_file import print_file
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QImage, QPixmap
+from PySide6.QtCore import QTimer
 
-app = QApplication()
-window = QWidget()
-window.show()
 
-cap = cv2.VideoCapture(0)
-while True:
-    _, frame = cap.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-    laplacian = cv2.Laplacian(blurred_frame, cv2.CV_64F)
-    canny = cv2.Canny(blurred_frame, 100, 150)
-    cv2.imshow("Frame", frame)
+class MainApp(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.video_size = QSize(320, 240)
+        self.setup_ui()
+        self.setup_camera()
 
-    key = cv2.waitKey(1)   
-    if key == 27: #27번 key는 esc
-        break
-    if key == 13:
-        cv2.imwrite('photo.jpg', frame)
-        print_file('photo.jpg')
-        break
-    
-app.exec()
-cap.release()
-cv2.destroyAllWindows()
+    def print_photo(self):
+        cv2.imwrite('test.jpg', cv2.VideoCapture(0).read()[1])
+        print_file('test.jpg')
 
+    def setup_ui(self):
+        self.image_label = QLabel()
+        self.image_label.setFixedSize(self.video_size)
+
+        self.photo_button = QPushButton("찰칵!")
+        self.photo_button.clicked.connect(self.print_photo)
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addWidget(self.image_label)
+        self.main_layout.addWidget(self.photo_button)
+
+        self.setLayout(self.main_layout)
+
+    def setup_camera(self):
+        self.capture = cv2.VideoCapture(0)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.display_video_stream)
+        self.timer.start(30)
+
+    def display_video_stream(self):
+        _, frame = self.capture.read()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = cv2.flip(frame, 1)
+        image = QImage(frame, frame.shape[1], frame.shape[0],
+                       frame.strides[0], QImage.Format_RGB888)
+        self.image_label.setPixmap(QPixmap.fromImage(image))
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    win = MainApp()
+    win.show()
+    sys.exit(app.exec_())
